de85368ccd88ddeaf10eaf3ee6506a3416e83e62
marquis-codes <abahmarquis@gmail.com>
2026-03-13 00:42:39 +0100

    forcefully

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     ],
     runtimeConfig: {
         public: {
-            apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3000/api/v1',
+            apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://wisekings-backend-hq.onrender.com/api/v1',
         },
     },
     app: {
